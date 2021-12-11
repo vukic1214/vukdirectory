414623517e74a43f9f363ff14a0ef24784c52629
vukic1214 <95051736+vukic1214@users.noreply.github.com>
2021-12-11 14:45:50 +0100

    Create publish.bat

---
+++ publish.bat
@@ -0,0 +1,7 @@
+echo "# vukdirectory" >> *.bat -enter here your directory and name of file
+git init
+git add *.bat
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/vukic1214/vukdirectory.git
+git push -u origin main
